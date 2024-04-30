5cc38765f7077cb547334b467e01785ac57c0d7e
Lucas Gurgel <gurgellucas461@gmail.com>
2024-04-29 23:23:06 -0300

    Update

--- Project Management 02/Library project/Biblioteca.java
+++ Project Management 02/Library project/Biblioteca.java
@@ -1,127 +1,62 @@
 import java.util.ArrayList;
-import java.util.Date;
 import java.io.Serializable;
 
+
 public class Biblioteca implements Serializable {
-    private ArrayList<Livro> listaLivros;
-    private ArrayList<Cliente> listaClientes;
-
-    public Biblioteca() {
-        this.listaLivros = new ArrayList<>();
-        this.listaClientes = new ArrayList<>();
-    }
-
-    public void adicionarLivro(Livro livro) {
-        listaLivros.add(livro);
-        // Adicionar o livro à sua categoria - aprimorar
-        livro.getCategoria().getLivros().add(livro);
-    }
-
-    public void removerLivro(Livro livro) {
-        listaLivros.remove(livro);
-        // Remover o livro de sua categoria
-        livro.getCategoria().getLivros().remove(livro);
-    }
-
-    public void adicionarCliente(Cliente cliente) {
-        listaClientes.add(cliente);
-    }
-
-    public void removerCliente(Cliente cliente) {
-        listaClientes.remove(cliente);
-    }
-
-    public void emprestarLivro(Livro livro, Cliente cliente) {
-        if (livro.isDisponivel()) {
-            livro.setDisponivel(false);
-            // Registrar o emprestimo
-            Emprestimo emprestimo = new Emprestimo(new Date(), cliente.getNome());
-            livro.adicionarEmprestimo(emprestimo);
-            cliente.adicionarLivroEmprestado(livro);
-            System.out.println("Livro emprestado com sucesso.");
-        } else {
-            System.out.println("Livro não disponível para empréstimo.");
-        }
-    }
-
-    public void devolverLivro(Livro livro, Cliente cliente) {
-        livro.setDisponivel(true);
-        // Remover o empréstimo
-        for (Emprestimo emprestimo : livro.getHistoricoEmprestimos()) {
-            if (emprestimo.getNomeUsuario().equals(cliente.getNome())) {
-                livro.removerEmprestimo(emprestimo);
-                cliente.removerLivroEmprestado(livro);
-                break;
-            }
-        }
-        System.out.println("Livro devolvido com sucesso.");
-    }
-
-    public void imprimirListaLivros(boolean disponiveis) {
-        System.out.println("\n=== Lista de Livros ===");
-        for (Livro livro : listaLivros) {
-            if (livro.isDisponivel() == disponiveis) {
-                String status = disponiveis ? "Disponível" : "Indisponível";
-                System.out.println("Nome: " + livro.getTitulo());
-                System.out.println("ID: " + livro.getIdLivro());
-                System.out.println("Gênero: " + livro.getCategoria().getNome());
-                System.out.println("Status: " + status);
-                System.out.println();
-            }
-        }
-    }
+   private static ArrayList<Livro> listaLivros = null;
+   private static ArrayList<Cliente> listaClientes = null;
 
 
-    public ArrayList<Livro> pesquisarLivrosPorGenero(String genero) {
-        ArrayList<Livro> livrosEncontrados = new ArrayList<>();
-        for (Livro livro : listaLivros) {
-            if (livro.getCategoria().getNome().equalsIgnoreCase(genero)) {
-                livrosEncontrados.add(livro);
-            }
-        }
-        return livrosEncontrados;
-    }
+   public Biblioteca() {
+       listaLivros = new ArrayList<>();
+       listaClientes = new ArrayList<>();
+   }
 
-    public void imprimirHistoricoEmprestimos(Livro livro) {
-        System.out.println("\n=== Histórico de Empréstimos do Livro ===");
-        for (Emprestimo emprestimo : livro.getHistoricoEmprestimos()) {
-            System.out.println("Data de Empréstimo: " + emprestimo.getDataEmprestimo());
-            System.out.println("Nome do Cliente: " + emprestimo.getNomeUsuario());
-            System.out.println();
-        }
-    }
 
-    public ArrayList<Livro> getListaLivros() {
-        return listaLivros;
-    }
+   public static void imprimirListaLivros(boolean disponiveis) {
+       System.out.println("\n=== Lista de Livros ===");
+       for (Livro livro : listaLivros) {
+           if (livro.isDisponivel() == disponiveis) {
+               String status = disponiveis ? "Disponível" : "Indisponível";
+               System.out.println("Nome: " + livro.getTitulo());
+               System.out.println("ID: " + livro.getIdLivro());
+               System.out.println("Curso: " + livro.getCategoria().getNome());
+               System.out.println("Status: " + status);
+               System.out.println();
+           }
+       }
+   }
 
-    public ArrayList<Cliente> getListaClientes() {
-        return listaClientes;
-    }
-    public Livro buscarLivroPorID(int idLivro) {
-        for (Livro livro : listaLivros) {
-            if (livro.getIdLivro() == idLivro) {
-                return livro;
-            }
-        }
-        return null; // se nao encontrar livro c ID
-    }
 
-    public Cliente buscarClientePorID(int idCliente) {
-        for (Cliente cliente : listaClientes) {
-            if (cliente.getIdCliente() == idCliente) {
-                return cliente;
-            }
-        }
-        return null; // se nao encontrar usuario c ID
-    }
+   public static void imprimirHistoricoLeitura() {
+       System.out.println("\n===== Histórico de Leitura do Usuário =====");
+       System.out.println("Livros lidos por " + Main.usuarioLogado.getNome() + ":");
 
-    public Livro buscarLivroPorId(int idLivro) {
-        for (Livro livro : listaLivros) {
-            if (livro.getIdLivro() == idLivro) {
-                return livro;
-            }
-        }
-        return null; // Retornar null se o livro não for encontrado
-    }
+
+       if (Main.usuarioLogado.getLivrosEmprestados().isEmpty()) {
+           System.out.println("O usuário ainda não leu nenhum livro.");
+           return;
+       }
+       for (Livro livro : Main.usuarioLogado.getLivrosEmprestados()) {
+           System.out.println("Título: " + livro.getTitulo());
+           System.out.println("ID: " + livro.getIdLivro());
+           System.out.println("Gênero: " + livro.getCategoria().getNome());
+           System.out.println("Status: " + (livro.isDisponivel() ? "Disponível" : "Indisponível"));
+           System.out.println();
+       }
+   }
+   public static ArrayList<Livro> getListaLivros() {
+       return listaLivros;
+   }
+   public static ArrayList<Cliente> getListaClientes() {
+       return listaClientes;
+   }
+   public static Livro buscarLivroPorID(int idLivro) {
+       for (Livro livro : listaLivros) {
+           if (livro.getIdLivro() == idLivro) {
+               return livro;
+           }
+       }
+       return null; // se nao encontrar livro c ID
+   }
 }
--- Project Management 02/Library project/Pessoa.java
+++ Project Management 02/Library project/Pessoa.java
@@ -2,164 +2,328 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 
-
 class Pessoa implements Serializable {
-    public String nome;
-    private String endereco;
-    private String telefone;
-
-    public Pessoa(String nome, String endereco, String telefone) {
-        this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void imprimirDetalhes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Endereço: " + endereco);
-        System.out.println("Telefone: " + telefone);
-    }
-}
+   public String nome;
 
 
-
-class Cliente extends Pessoa {
-    private int idCliente;
-    private String senha;
-    private final ArrayList<Livro> livrosEmprestados;
-
-    public Cliente(String nome, String endereco, String telefone, String senha, int idCliente) {
-        super(nome, endereco, telefone);
-        this.senha = senha;
-        this.idCliente = idCliente;
-        this.livrosEmprestados = new ArrayList<>();
-    }
-
-    // senha
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public int getIdCliente() {
-        return idCliente;
-    }
-
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente;
-    }
-    public ArrayList<Emprestimo> getHistoricoEmprestimos() {
-        // Lógica para retornar o histórico de empréstimos do cliente
-        // Implemente de acordo com a sua aplicação
-        return null; // Temporário, substitua com a lógica real
-    }
-
-    public ArrayList<Livro> getLivrosEmprestados() {
-        return livrosEmprestados;
-    }
-
-    public void adicionarLivroEmprestado(Livro livro) {
-        livrosEmprestados.add(livro);
-    }
-
-    public void removerLivroEmprestado(Livro livro) {
-        livrosEmprestados.remove(livro);
-    }
-
-    public boolean verificarSenha(String senha) {
-        return this.senha.equals(senha);
-    }
-    public String getNomeUsuario() {
-        return this.nome;
-    }
+public Pessoa(String nome) {
+   this.nome = nome;
 }
 
 
 
 
-class Funcionario extends Pessoa {
-
-    // Atributo específico do Funcionário
-    private int idFuncionario;
-
-    // Construtor
-    public Funcionario(String nome, String endereco, String telefone, int idFuncionario) {
-        super(nome, endereco, telefone);
-        this.idFuncionario = idFuncionario;
-    }
-
-    // Getters e Setters específicos do Funcionário
-    public int getIdFuncionario() {
-        return idFuncionario;
-    }
-
-    public void setIdFuncionario(int idFuncionario) {
-        this.idFuncionario = idFuncionario;
-    }
-
-    // Método para adicionar um novo livro ao sistema
-    public void adicionarLivro(Biblioteca biblioteca, Livro livro) {
-        biblioteca.adicionarLivro(livro);
-    }
-
-    // Método para remover um livro do sistema
-    public void removerLivro(Biblioteca biblioteca, Livro livro) {
-        biblioteca.removerLivro(livro);
-    }
-
-    // Método para atualizar informações de um livro no sistema
-    public void atualizarLivro(Livro livro, String titulo, String autor, int idLivro, Categoria categoria, boolean disponivel) {
-        livro.setTitulo(titulo);
-        livro.setAutor(autor);
-        livro.setIdLivro(idLivro);
-        livro.setCategoria(categoria);
-        livro.setDisponivel(disponivel);
-    }
-
-    public void verHistoricoEmprestimos(Livro livro) {
-        ArrayList<Emprestimo> historicoEmprestimos = livro.getHistoricoEmprestimos();
-
-        if (historicoEmprestimos.isEmpty()) {
-            System.out.println("Este livro não possui histórico de empréstimos.");
-        } else {
-            System.out.println("\n===== Histórico de Empréstimos do Livro =====");
-            for (Emprestimo emprestimo : historicoEmprestimos) {
-                System.out.println("Data de Empréstimo: " + emprestimo.getDataEmprestimo());
-                System.out.println("Nome do Cliente: " + emprestimo.getNomeUsuario());
-                System.out.println();
-            }
-        }
-    }
+   public String getNome() {
+       return nome;
+   }
+}
+class Cliente extends Pessoa implements Serializable {
+   private final int idCliente;
+   private final String senha;
+   private final ArrayList<Livro> livrosEmprestados;
 
 
+   public Cliente(String nome, String senha, int idCliente) {
+       super(nome);
+       this.senha = senha;
+       this.idCliente = idCliente;
+       this.livrosEmprestados = new ArrayList<>();
+   }
+
+
+   public static void adicionarCliente(Cliente cliente) {
+       Biblioteca.getListaClientes().add(cliente);
+   }
+
+
+   public static void menuUsuario() {
+       System.out.println("\n===== Menu Aluno =====");
+       System.out.println("1. Fazer login");
+       System.out.println("2. Cadastrar");
+       System.out.println("3. Voltar");
+       System.out.print("Escolha uma opção: ");
+
+
+       int opcao = Main.scanner.nextInt();
+
+
+       switch (opcao) {
+           case 1:
+               fazerLoginUsuario();
+               break;
+           case 2:
+               cadastrarUsuario();
+               break;
+           case 3:
+               System.out.println("Voltando ao menu inicial...");
+               break;
+           default:
+               System.out.println("Opção inválida. Escolha novamente.");
+       }
+   }
+
+
+   public static void fazerLoginUsuario() {
+       System.out.println("\n===== Login de Aluno =====");
+       System.out.print("Nome de usuário: ");
+       String nomeUsuario = Main.scanner.next().toLowerCase();
+       System.out.print("Senha: ");
+       String senha = Main.scanner.next();
+
+
+       // Verificar o login de usuário
+       Main.usuarioLogado = verificarLoginUsuario(nomeUsuario, senha);
+       if (Main.usuarioLogado != null) {
+           menuOperacoesUsuario();
+       } else {
+           System.out.println("Usuário ou senha incorretos. Tente novamente.");
+       }
+   }
+
+
+   public static Cliente verificarLoginUsuario(String nomeUsuario, String senha) {
+       String nomeUsuarioLower = nomeUsuario.toLowerCase(); // Converter para minúsculas
+       for (Cliente cliente : Biblioteca.getListaClientes()) {
+           if (cliente.getNome().toLowerCase().equals(nomeUsuarioLower) && cliente.verificarSenha(senha)) {
+               return cliente;
+           }
+       }
+       return null;
+   }
+
+
+
+
+   public static void cadastrarUsuario() {
+       System.out.println("\n===== Cadastro do Aluno =====");
+       System.out.print("Nome de usuário: ");
+       String nome = Main.scanner.next();
+
+
+       // Verifica se o nome de usuário já existe (sem diferenciar maiúsculas e minúsculas)
+       for (Cliente cliente : Biblioteca.getListaClientes()) {
+           if (cliente.getNome().equalsIgnoreCase(nome)) {
+               System.out.println("Este nome de usuário já está em uso. Escolha outro.");
+               return; // Retorna ao menu
+           }
+       }
+       System.out.print("Senha: ");
+       String senha = Main.scanner.next();
+
+
+       // ID único para o novo usuário
+       int idUsuario = Utils.gerarIDAleatorio();
+
+
+       Cliente novoCliente = new Cliente(nome, senha, idUsuario);
+
+
+       adicionarCliente(novoCliente); // Adiciona o novo cliente à lista de clientes da biblioteca
+
+
+       System.out.println("Usuário cadastrado com sucesso! Seu ID é: " + idUsuario);
+   }
+
+
+   public static void menuOperacoesUsuario() {
+       int opcao;
+       // Loop principal do menu de operações do usuário
+       do {
+           System.out.println("\n===== Menu de Opções do Aluno =====");
+           System.out.println("1. Emprestimo de livro");
+           System.out.println("2. Devolver livro");
+           System.out.println("3. Lista de livros");
+           System.out.println("4. Pesquisar livro por gênero");
+           System.out.println("5. Histórico de empréstimos");
+           System.out.println("6. Ver ID do aluno");
+           System.out.println("7. Livros mais bem avaliados");
+           System.out.println("8. Fazer Logout");
+           System.out.print("Escolha uma opção: ");
+           opcao = Main.scanner.nextInt();
+
+
+           switch (opcao) {
+               case 1:
+                   Emprestimo.emprestarLivro();
+                   break;
+               case 2:
+                   Emprestimo.devolverLivro();
+                   break;
+               case 3:
+                   System.out.println("\n=== Lista de Livros Disponíveis ===");
+                   Biblioteca.imprimirListaLivros(true); // imprime apenas livros disponíveis
+                   System.out.println("\n=== Lista de Livros Indisponíveis ===");
+                   Biblioteca.imprimirListaLivros(false); // Imprime apenas livros indisponíveis
+                   break;
+               case 4:
+                   Categoria.pesquisarLivroPorCurso();
+                   break;
+               case 5:
+                   System.out.println("\n===== Histórico =====");
+                   System.out.println("Escolha uma opção:");
+                   System.out.println("1. Histórico de empréstimos de livros");
+                   System.out.println("2. Histórico de leitura do usuário");
+                   System.out.print("Opção: ");
+                   int opcaoHistorico = Main.scanner.nextInt();
+                   switch (opcaoHistorico) {
+                       case 1:
+                           Emprestimo.imprimirHistoricoEmprestimos();
+                           break;
+                       case 2:
+                           Biblioteca.imprimirHistoricoLeitura();
+                           break;
+                       default:
+                           System.out.println("Opção inválida.");
+                   }
+                   break;
+               case 6:
+                   System.out.println("Seu ID de usuário é: " + Main.usuarioLogado.getIdCliente());
+                   break;
+               case 7:
+                   Livro.listarLivrosMaisBemAvaliados();
+                   break;
+               case 8:
+                   Main.usuarioLogado = null; // Faz logout, limpando o usuário logado
+                   System.out.println("Voltando ao menu inicial...");
+                   break;
+               default:
+                   System.out.println("Opção inválida. Por favor, escolha novamente.");
+
+
+           }
+       } while (opcao != 8); // continua no loop
+
+
+       System.out.print("Digite 'easter' para um Easter Egg: ");
+       String easterInput = Main.scanner.next().toLowerCase();
+       if (easterInput.equals("easter")) {
+           ativarEasterEgg();
+       }
+   }
+
+
+
+
+   private static void ativarEasterEgg() {
+       System.out.println("Surpresa! Você encontrou o Easter Egg!");
+       System.out.println("Parabéns! Aqui está uma piada para você:");
+       System.out.println("Por que o programador foi preso durante as férias?");
+       System.out.println("Porque ele deletou todos os seus cookies!");
+       // Você pode adicionar mais interações especiais aqui
+   }
+
+
+   public int getIdCliente() {
+       return idCliente;
+   }
+
+
+   public ArrayList<Livro> getLivrosEmprestados() {
+       return livrosEmprestados;
+   }
+
+
+   public boolean verificarSenha(String senha) {
+       return this.senha.equals(senha);
+   }
 }
 
 
 
+
+class Funcionario extends Pessoa implements Serializable {
+   public Funcionario(String nome) {
+       super(nome);
+   }
+
+
+   public static void cadastrarFuncionario() {
+       System.out.println("Favor entrar em contato diretamente com a direção.");
+   }
+
+
+   public static void menuOperacoesFuncionario() {
+       int opcao;
+       do {
+           System.out.println("\n===== Menu de Operações do Funcionário =====");
+           System.out.println("1. Adicionar Livro");
+           System.out.println("2. Remover Livro");
+           System.out.println("3. Atualizar Nome do Livro");
+           System.out.println("4. Ver Livros Emprestados");
+           System.out.println("5. Ver Avaliações dos Livros");
+           System.out.println("6. Fazer Logout");
+           System.out.print("Escolha uma opção: ");
+           opcao = Main.scanner.nextInt();
+
+
+
+
+           switch (opcao) {
+               case 1:
+                   Livro.cadastrarLivro();
+                   break;
+               case 2:
+                   Livro.removerLivro();
+                   break;
+               case 3:
+                   Livro.atualizarLivro();
+                   break;
+               case 4:
+                   Emprestimo.verLivrosEmprestados();
+                   break;
+               case 5:
+                   Livro.verAvaliacoesLivros();
+                   break;
+               case 6:
+                   System.out.println("Saindo");
+                   break;
+               default:
+                   System.out.println("Opção inválida. Escolha novamente.");
+           }
+       } while (opcao != 6);
+   }
+
+
+
+
+   public static void menuFuncionario() {
+       System.out.println("\n===== Menu Funcionário =====");
+       System.out.println("1. Fazer login");
+       System.out.println("2. Cadastrar");
+       System.out.println("3. Voltar");
+       System.out.print("Escolha uma opção: ");
+       int opcao = Main.scanner.nextInt();
+
+
+
+
+       switch (opcao) {
+           case 1:
+               fazerLoginFuncionario();
+               break;
+           case 2:
+               Funcionario.cadastrarFuncionario();
+               break;
+           case 3:
+               System.out.println("Voltando ao menu inicial...");
+               break;
+           default:
+               System.out.println("Opção inválida. Por favor, escolha novamente.");
+       }
+   }
+
+
+   public static void fazerLoginFuncionario() {
+       System.out.println("\n===== Login de Funcionário =====");
+       System.out.print("Senha: ");
+       String senha = Main.scanner.next();
+
+
+       if (senha.equals("UNICAP2024")) {
+           System.out.println("Login de funcionário bem-sucedido!");
+           Funcionario.menuOperacoesFuncionario();
+       } else {
+           System.out.println("Senha incorreta. Tente novamente.");
+       }
+   }
+}
